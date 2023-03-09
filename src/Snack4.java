5f04efcf7bd73e851a95384d39e68b755a632ae3
ttom-eragiapreso <109075301+ttom-eragiapreso@users.noreply.github.com>
2023-03-09 15:14:29 +0100

    snack 4 finished

--- src/Snack4.java
+++ src/Snack4.java
@@ -7,6 +7,12 @@ public class Snack4 {
     * nell’array che ne ha di meno + quelli mostrati a video non è uguale al numero di elementi
     * presenti nell’array che ne ha di più*/
 
-
+        int[] numeri1 = new int[5];
+        int[] numeri2 = new int[10];
+        int min = 1;
+        int max = 15;
+        for (int i = 0; i < numeri2.length - numeri1.length; i++) {
+            System.out.println((int)Math.ceil(Math.random() * (max - min) + min));
+        }
     }
 }
