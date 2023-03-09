649810cac84280db2a1dfefb84a2340db44d9e4b
ttom-eragiapreso <109075301+ttom-eragiapreso@users.noreply.github.com>
2023-03-09 15:42:15 +0100

    snack 4 using Random

--- src/Snack2.java
+++ src/Snack2.java
@@ -1,20 +1,25 @@
+import java.util.Random;
+
 public class Snack2 {
     public static void main(String[] args) {
 
 
         /*Snack 2
         Generatore di “nomi cognomi” casuali: il Grande Gatsby ha una lista di nomi e una lista di cognomi,
-        e da queste vuole mostrare a video una falsa lista di invitati con nome e cognome.*/
+        e da queste vuole mostrare a video una falsa lista d'invitati con nome e cognome.*/
         //Lista di nomi e cognomi
         String[] nomi = {"Mario", "Luigi", "Anna", "Luca", "Fabio"};
         String[] cognomi = {"Rossi", "Ferrari", "Russo", "Bianchi", "Romano"};
         //Limite per la generazione del numero random
-        int min = 0;
         int max = 4;
-        //Ad ogni ciclo prende un elemento a caso da ogni lista e li concatena.
+
+
+        Random rnd = new Random();
+
+        //A ogni ciclo prende un elemento a caso da ogni lista e li concatena.
         for (int i = 0; i < 5; i++) {
-            String randomName = nomi[(int)Math.ceil(Math.random() * (max - min) + min)];
-            String randomSurname = cognomi[(int)Math.ceil(Math.random() * (max - min) + min)];
+            String randomName = nomi[rnd.nextInt(max)];
+            String randomSurname = cognomi[rnd.nextInt(max)];
             System.out.println(randomName + " " + randomSurname);
         }
     }
