52da47e4032aeb044dd526e0b37045a5b36f3957
ttom-eragiapreso <109075301+ttom-eragiapreso@users.noreply.github.com>
2023-03-09 14:16:04 +0100

    snack 1 finished

---
+++ src/Snack1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Snack1 {
+    public static void main(String[] args) {
+
+        /*Snack 1
+        Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.*/
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Inserisci un numero");
+        int n = Integer.parseInt(sc.nextLine());
+
+        System.out.println(n % 2 == 0? n : n+1);
+
+
+    }
+}
